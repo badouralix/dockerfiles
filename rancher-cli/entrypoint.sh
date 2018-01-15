75b809eb5e822be8466df4d96feb906431c137c8
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2018-01-15 10:50:56 +0100

    (rancher-cli) fixing type

--- rancher-cli/entrypoint.sh
+++ rancher-cli/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Prepend "rnacher" if the first argument is not an executable
+# Prepend "rancher" if the first argument is not an executable
 if ! type -- "$1" &> /dev/null; then
 	set -- rancher "$@"
 fi
